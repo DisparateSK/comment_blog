3fcab8159eacc1edcb659949dac7f7e293fcacfb
Sergey <102255416+DisparateSK@users.noreply.github.com>
2022-05-21 19:22:06 +0300

    Add gunicorn

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ Base = declarative_base()
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
