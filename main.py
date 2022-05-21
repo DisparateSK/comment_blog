5c13f72fb9d1f5344c5663f1266ec860dbe9f730
Sergey <102255416+DisparateSK@users.noreply.github.com>
2022-05-21 19:54:56 +0300

    Correct uri

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
