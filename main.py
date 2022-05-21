85b14e53bfa76865bea6bb442d961b8e19be5f90
Sergey <102255416+DisparateSK@users.noreply.github.com>
2022-05-21 19:51:03 +0300

    Add gunicorn

--- main.py
+++ main.py
@@ -23,6 +23,11 @@ Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
                     base_url=None)
 ##CONNECT TO DB
+uri = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
